a567f378dcc58785d40572d33bc74185256d7383
George Vauter <george.vauter@inin.com>
2017-05-31 20:41:25 -0400

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_reqs = [
 ]
 
 setup(name='threatstack',
-      version='1.0.0',
+      version='1.0.1',
       description='A Python client for the Threat Stack API',
       license='MIT License',
       author='PureCloud Security Team',
