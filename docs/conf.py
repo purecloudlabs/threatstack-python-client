1fa3c3aaf46da10a0852431a2e13fe38d1b1a4e5
George Vauter <george.vauter@inin.com>
2017-09-18 11:52:57 -0400

    add v2 docs

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ author = u'Genesys | Interactive Intelligence, Inc.'
 # built documents.
 #
 # The short X.Y version.
-version = u'1.0.4'
+version = u'1.1.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
