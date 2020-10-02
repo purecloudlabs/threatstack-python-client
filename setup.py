678473c2504d6ec9bde5da9bff109b7d514ba35a
dannyrappleyea <dannyrappleyea@users.noreply.github.com>
2020-10-02 09:29:52 -0400

    Update setup.py
    Update to Genesys Cloud branding

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ install_reqs = [
 ]
 
 setup(name='threatstack',
-      version='1.2.0',
+      version='1.2.1',
       description='A Python client for the Threat Stack API',
       license='MIT License',
-      author='PureCloud Security Team',
-      author_email='george.vauter@genesys.com',
+      author='Genesys Cloud Security Team',
+      author_email='danny.rappleyea@genesys.com',
       maintainer='Genesys Labs',
-      url='https://github.com/MyPureCloud/threatstack-python-client',
+      url='https://github.com/purecloudlabs/threatstack-python-client',
       install_requires=install_reqs,
       classifiers=[
           'Development Status :: 4 - Beta',
